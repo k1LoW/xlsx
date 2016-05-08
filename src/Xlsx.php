d70df77a564a48d0a3edba0f287dc7c2db4d8e90
k1LoW <k1lowxb@gmail.com>
2016-05-08 15:26:52 +0900

    Initial commit

---
+++ src/Xlsx.php
@@ -0,0 +1,323 @@
+<?php
+
+namespace Xlsx;
+
+if (file_exists(dirname(__FILE__).'/../vendor/autoload.php')) {
+    require_once dirname(__FILE__).'/../vendor/autoload.php';
+}
+
+use PHPExcel;
+use PHPExcel_IOFactory;
+use PHPExcel_Style_Fill;
+
+/**
+ * Xlsx.
+ */
+class Xlsx
+{
+    public $xlsx;
+    private $data;
+
+    public function __construct($templateFilePath = null)
+    {
+        $this->xlsx = null;
+        $this->data = [];
+        if (file_exists($templateFilePath)) {
+            $this->read($templateFilePath);
+        }
+    }
+
+    /**
+     * __call.
+     *
+     * @param $method, $args
+     */
+    public function __call($method, $args)
+    {
+        if (!$this->xlsx) {
+            return;
+        }
+
+        return call_user_func_array([$this->xlsx, $method], $args);
+    }
+
+    /**
+     * read.
+     */
+    public function read($filePath)
+    {
+        $xlsReader = PHPExcel_IOFactory::createReaderForFile($filePath);
+        $this->type = preg_replace('/^.+_/', '', get_class($xlsReader));
+        $this->xlsx = $xlsReader->load($filePath);
+
+        return $this;
+    }
+
+    /**
+     * set.
+     */
+    public function set($key, $value = null)
+    {
+        if (is_array($key)) {
+            foreach ($key as $k => $v) {
+                $this->data[$k] = (string) $v;
+            }
+        } else {
+            $this->data[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    /**
+     * write.
+     */
+    public function write($outputFilePath, $data = [])
+    {
+        if (!empty($data)) {
+            $this->set($data);
+        }
+        if (!$this->xlsx) {
+            $this->read($outputFilePath);
+        }
+
+        $sheets = $this->xlsx->getAllSheets();
+
+        if (!empty($this->data)) {
+            foreach ($sheets as $key => $sheet) {
+                // Replace sheet title
+                $title = $sheet->getTitle();
+                if ($this->__replace($title)) {
+                    $sheet->setTitle($this->__replace($title));
+                }
+
+                $rMax = $sheet->getHighestRow();
+                $cMax = $sheet->getHighestColumn();
+                for ($r = 1; $r <= $rMax; ++$r) {
+                    for ($c = 0; $c <= self::alphabetToNumber($cMax); ++$c) {
+                        $cell = $sheet->getCellByColumnAndRow($c, $r);
+                        $value = $cell->getValue();
+                        if (is_object($value)) {
+                            $value = $cell->getPlainText();
+                        }
+                        if ($this->__replace($value)) {
+                            $cell->setValue($this->__replace($value));
+                        }
+                    }
+                }
+            }
+        }
+
+        if (empty($this->type)) {
+            $this->type = $this->__getType($outputFilePath);
+        }
+
+        $xlsWriter = PHPExcel_IOFactory::createWriter($this->xlsx, $this->type);
+        $xlsWriter->save($outputFilePath);
+        if (!file_exists($outputFilePath)) {
+            throw new Exception();
+        }
+
+        return true;
+    }
+
+    /**
+     * output
+     * Output xls with header.
+     */
+    public function output($filename = 'output.xlsx', $data = [])
+    {
+        $outputFilePath = TMP.uniqid('xls_', true).$filename;
+        $this->write($outputFilePath, $data);
+        header('Pragma: public');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Content-Type: application/force-download');
+        header('Content-Type: application/octet-stream');
+        header('Content-Type: application/download');
+        header("Content-Disposition: attachment; filename=\"$filename\"");
+        header('Content-Transfer-Encoding: binary ');
+        ob_clean();
+        flush();
+        echo file_get_contents($outputFilePath);
+        exit;
+    }
+
+    /**
+     * setValue.
+     */
+    public function setValue($value, $option = ['col' => 'A',
+                                                'row' => '1',
+                                                'sheet' => 0, ])
+    {
+        if (!array_key_exists('col', $option)
+            || !array_key_exists('row', $option)
+        ) {
+            return false;
+        }
+        if (!array_key_exists('sheet', $option)) {
+            $option['sheet'] = 0;
+        }
+        if (empty($this->xlsx)) {
+            $this->xlsx = new PHPExcel();
+        }
+        $this->xlsx->setActiveSheetIndex($option['sheet']);
+        $sheet = $this->xlsx->getActiveSheet();
+        $sheet->setCellValueByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'], $value);
+
+        // border
+        if (array_key_exists('border', $option)) {
+            if (is_array($option['border'])) {
+                foreach (['top', 'right', 'left', 'bottom'] as $position) {
+                    if (array_key_exists($position, $option['border'])) {
+                        $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getBorders()->{'get'.ucfirst($position)}()->setBorderStyle($option['border'][$position]);
+                    }
+                }
+            } else {
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getBorders()->getAllBorders()->setBorderStyle($option['border']);
+            }
+        }
+
+        // align horizontal
+        if (array_key_exists('align', $option)) {
+            $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])
+                ->getAlignment()
+                ->setHorizontal($option['align']);
+        }
+        if (array_key_exists('hAlign', $option)) {
+            $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])
+                ->getAlignment()
+                ->setHorizontal($option['hAlign']);
+        }
+
+        // align vertical
+        if (array_key_exists('vAlign', $option)) {
+            $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])
+                ->getAlignment()
+                ->setVertical($option['vAlign']);
+        }
+
+        // font
+        if (array_key_exists('font', $option)) {
+            $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFont()->setName($option['font']);
+        }
+
+        // font color
+        if (array_key_exists('color', $option)) {
+            if (strlen($option['color']) === 8) {
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFont()->getColor()->setARGB($option['color']);
+            } elseif (strlen($option['color']) === 6) {
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFont()->getColor()->setRGB($option['color']);
+            }
+        }
+
+        // font size
+        if (array_key_exists('size', $option)) {
+            $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFont()->setSize($option['size']);
+        }
+
+        // backgroundColor / backgroundType
+        if (array_key_exists('backgroundColor', $option)) {
+            $type = empty($option['backgroundType']) ? PHPExcel_Style_Fill::FILL_SOLID : $option['backgroundType'];
+            if (strlen($option['backgroundColor']) === 8) {
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFill()->setFillType($type);
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFill()->getStartColor()->setARGB($option['backgroundColor']);
+            } elseif (strlen($option['backgroundColor']) === 6) {
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFill()->setFillType($type);
+                $sheet->getStyleByColumnAndRow(self::alphabetToNumber($option['col']), $option['row'])->getFill()->getStartColor()->setRGB($option['backgroundColor']);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * alphabetToNumber.
+     */
+    public static function alphabetToNumber($value)
+    {
+        if (is_numeric($value)) {
+            return $value;
+        }
+        $alphabet = array_flip(str_split('abcdefghijklmnopqrstuvwxyz'));
+        $strArray = array_reverse(str_split(strtolower($value)));
+        $number = 0;
+        foreach ($strArray as $n => $str) {
+            if ($n == 0) {
+                $number += $alphabet[$str];
+            } else {
+                $number += ($alphabet[$str] + 1) * pow(26, $n);
+            }
+        }
+
+        return $number;
+    }
+
+    /**
+     * __getType.
+     *
+     * @see IOFactory::createReaderForFile
+     */
+    private function __getType($filePath)
+    {
+        $pathinfo = pathinfo($filePath);
+
+        if (!isset($pathinfo['extension'])) {
+            return false;
+        }
+        switch (strtolower($pathinfo['extension'])) {
+        case 'xlsx':            //  Excel (OfficeOpenXML) Spreadsheet
+        case 'xlsm':            //  Excel (OfficeOpenXML) Macro Spreadsheet (macros will be discarded)
+        case 'xltx':            //  Excel (OfficeOpenXML) Template
+        case 'xltm':            //  Excel (OfficeOpenXML) Macro Template (macros will be discarded)
+            $extensionType = 'Excel2007';
+            break;
+        case 'xls':             //  Excel (BIFF) Spreadsheet
+        case 'xlt':             //  Excel (BIFF) Template
+            $extensionType = 'Excel5';
+            break;
+        case 'ods':             //  Open/Libre Offic Calc
+        case 'ots':             //  Open/Libre Offic Calc Template
+            $extensionType = 'OOCalc';
+            break;
+        case 'slk':
+            $extensionType = 'SYLK';
+            break;
+        case 'xml':             //  Excel 2003 SpreadSheetML
+            $extensionType = 'Excel2003XML';
+            break;
+        case 'gnumeric':
+            $extensionType = 'Gnumeric';
+            break;
+        case 'htm':
+        case 'html':
+            $extensionType = 'HTML';
+            break;
+        case 'csv':
+            // Do nothing
+            // We must not try to use CSV reader since it loads
+            // all files including Excel files etc.
+            return false;
+            break;
+        default:
+            break;
+        }
+
+        return $extensionType;
+    }
+
+    /**
+     * __replace.
+     */
+    private function __replace($value)
+    {
+        if (empty($value)) {
+            return false;
+        }
+        if (array_key_exists((string) $value, $this->data)) {
+            return $this->data[$value];
+        } else {
+            return false;
+        }
+    }
+}
---
+++ tests/XlsxTest.php
@@ -0,0 +1,298 @@
+<?php
+
+namespace Xlsx\tests;
+
+use Xlsx\Xlsx;
+use PHPExcel_Style_Border;
+use PHPExcel_Style_Color;
+
+class XlsxTest extends \PHPUnit_Framework_TestCase
+{
+    public $xlsx;
+
+    /**
+     * setUp.
+     */
+    public function setUp()
+    {
+    }
+
+    /**
+     * tearDown.
+     */
+    public function tearDown()
+    {
+        unset($this->xlsx);
+    }
+
+    /**
+     * testWriteExcel2007.
+     */
+    public function testWriteExcel2007()
+    {
+        $fileName = 'testbook.xlsx';
+        $this->inputFilePath = '/tmp/'.$fileName;
+        $this->outputFilePath = '/tmp/output.xlsx';
+        $this->_setTestFile($fileName, $this->inputFilePath);
+
+        $this->xlsx = new Xlsx();
+        $result = $this->xlsx->read($this->inputFilePath)
+            ->setValue('testset', ['col' => 'B', // jpn: col / row / sheetを指定してセット可能
+                                        'row' => '10',
+                                        ])
+            ->setValue('testset_with_sheet', ['col' => 'B', // jpn: col / row / sheetを指定してセット可能
+                                                   'row' => '10',
+                                                   'sheet' => 2,
+                                                   ])
+            ->setValue('testset_with_border', ['col' => 'C', // jpn: borderを指定してセット可能
+                                                    'row' => '10',
+                                                    'border' => ['top' => PHPExcel_Style_Border::BORDER_THICK,
+                                                                      'right' => PHPExcel_Style_Border::BORDER_MEDIUM,
+                                                                      'left' => PHPExcel_Style_Border::BORDER_THIN,
+                                                                      'bottom' => PHPExcel_Style_Border::BORDER_DOUBLE,
+                                                                      ],
+                                                    ])
+            ->setValue('testset_with_border', ['col' => 'E',
+                                                    'row' => '10',
+                                                    'border' => PHPExcel_Style_Border::BORDER_THICK,  // jpn: borderを一括指定可能
+                                                    ])
+            ->setValue('testset_with_color', ['col' => 'F', // jpn: colorを指定可能
+                                                   'row' => '10',
+                                                   'color' => PHPExcel_Style_Color::COLOR_BLUE,
+                                                   ])
+            ->setValue('testset_with_backgroud_color', ['col' => 'G', // jpn: backgroundColorを指定可能
+                                                             'row' => '10',
+                                                             'backgroundColor' => PHPExcel_Style_Color::COLOR_YELLOW,
+                                                             ])
+            ->set(['Sheet1' => 'シートタイトル', // jpn: 文字列置換でセット可能
+                        'test' => 'replaced',
+                        '4' => 5, ])
+            ->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testWriteExcel5.
+     */
+    public function testWriteExcel5()
+    {
+        $fileName = 'testbook.xls';
+        $this->inputFilePath = '/tmp/'.$fileName;
+        $this->outputFilePath = '/tmp/output.xls';
+        $this->_setTestFile($fileName, $this->inputFilePath);
+
+        $this->xlsx = new Xlsx($this->inputFilePath);
+        // jpn: col / row / sheetを指定してセット可能
+        $this->xlsx->setValue('testset', ['col' => 'B', // jpn: col / row / sheetを指定してセット可能
+                                              'row' => '10',
+                                              ])
+            ->setValue('testset_with_sheet', ['col' => 'B', // jpn: col / row / sheetを指定してセット可能
+                                                   'row' => '10',
+                                                   'sheet' => 2,
+                                                   ])
+            ->setValue('testset_with_border', ['col' => 'C', // jpn: borderを指定可能
+                                                    'row' => '10',
+                                                    'border' => ['top' => PHPExcel_Style_Border::BORDER_THICK,
+                                                                      'right' => PHPExcel_Style_Border::BORDER_MEDIUM,
+                                                                      'left' => PHPExcel_Style_Border::BORDER_THIN,
+                                                                      'bottom' => PHPExcel_Style_Border::BORDER_DOUBLE,
+                                                                      ],
+                                                    ])
+            ->setValue('testset_with_border', ['col' => 'E',
+                                                    'row' => '10',
+                                                    'border' => PHPExcel_Style_Border::BORDER_THICK,  // jpn: borderを一括指定可能
+                                                    ])
+            ->setValue('testset_with_color', ['col' => 'F', // jpn: colorを指定可能
+                                                   'row' => '10',
+                                                   'color' => PHPExcel_Style_Color::COLOR_BLUE,
+                                                   ])
+            ->setValue('testset_with_backgroud_color', ['col' => 'G', // jpn: backgroundColorを指定可能
+                                                             'row' => '10',
+                                                             'backgroundColor' => PHPExcel_Style_Color::COLOR_YELLOW,
+                                                             ])
+            ->set(['Sheet1' => 'シートタイトル',         // jpn: 文字列置換でセット可能
+                        'test' => 'replaced',
+                        '4' => 5, ]);
+        $result = $this->xlsx->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testNewExcel2007.
+     */
+    public function testNewExcel2007()
+    {
+        $this->outputFilePath = '/tmp/outputnew.xlsx';
+
+        $this->xls = new Xlsx();
+        $result = $this->xls
+            ->setValue('testset', ['col' => 'B', // jpn: col / row / sheetを指定してセット可能
+                                        'row' => '10',
+                                        ])
+            ->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testNewExcel5.
+     */
+    public function testNewExcel5()
+    {
+        $this->outputFilePath = '/tmp/outputnew.xls';
+
+        $this->xls = new Xlsx();
+        $result = $this->xls
+            ->setValue('testset', ['col' => 'B', // jpn: col / row / sheetを指定してセット可能
+                                        'row' => '10',
+                                        ])
+            ->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testSetValueManyExcel2007.
+     *
+     * @param
+     */
+    public function testSetValueManyExcel2007()
+    {
+        $fileName = 'testbook.xlsx';
+        $this->inputFilePath = '/tmp/'.$fileName;
+        $this->outputFilePath = '/tmp/'.'outputmany.xlsx';
+        $this->_setTestFile($fileName, $this->inputFilePath);
+
+        $this->xls = new Xlsx($this->inputFilePath);
+
+        for ($c = 0; $c < 100; ++$c) {
+            for ($r = 1; $r < 100; ++$r) {
+                $this->xls->setValue('testset', ['col' => $c,
+                                                      'row' => $r,
+                                                      ]);
+            }
+        }
+        $result = $this->xls->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testSetValueManyExcel5.
+     *
+     * @param
+     */
+    public function testSetValueManyExcel5()
+    {
+        $fileName = 'testbook.xls';
+        $this->inputFilePath = '/tmp/'.$fileName;
+        $this->outputFilePath = '/tmp/'.'outputmany.xls';
+        $this->_setTestFile($fileName, $this->inputFilePath);
+
+        $this->xls = new Xlsx($this->inputFilePath);
+
+        for ($c = 0; $c < 100; ++$c) {
+            for ($r = 1; $r < 100; ++$r) {
+                $this->xls->setValue('testset', ['col' => $c,
+                                                      'row' => $r,
+                                                      ]);
+            }
+        }
+        $result = $this->xls->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testSetManyExcel2007.
+     */
+    public function testSetManyExcel2007()
+    {
+        $fileName = 'testbookmany.xlsx';
+        $this->inputFilePath = '/tmp/'.$fileName;
+        $this->outputFilePath = '/tmp/'.'outputsetmany.xlsx';
+        $this->_setTestFile($fileName, $this->inputFilePath);
+
+        $this->xls = new Xlsx($this->inputFilePath);
+        for ($c = 'a'; Xlsx::alphabetToNumber($c) < 100; ++$c) {
+            for ($r = 1; $r < 100; ++$r) {
+                $this->xls->set('$'.strtoupper($c).'$'.$r, 'replaced');
+            }
+        }
+
+        $result = $this->xls->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testSetManyExcel5.
+     */
+    public function testSetManyExcel5()
+    {
+        $fileName = 'testbookmany.xls';
+        $this->inputFilePath = '/tmp/'.$fileName;
+        $this->outputFilePath = '/tmp/'.'outputsetmany.xls';
+        $this->_setTestFile($fileName, $this->inputFilePath);
+        $this->xls = new Xlsx($this->inputFilePath);
+        for ($c = 'a'; Xlsx::alphabetToNumber($c) < 100; ++$c) {
+            for ($r = 1; $r < 100; ++$r) {
+                $this->xls->set('$'.strtoupper($c).'$'.$r, 'replaced');
+            }
+        }
+
+        $result = $this->xls->write($this->outputFilePath);
+        $this->assertTrue($result);
+        echo 'Look '.$this->outputFilePath.PHP_EOL;
+        echo 'Peak memory usage: '.(memory_get_peak_usage(true) / 1024 / 1024).' MB'.PHP_EOL;
+    }
+
+    /**
+     * testAlphabetToNumber.
+     *
+     * @param $arg
+     */
+    public function testAlphabetToNumber()
+    {
+        $result = Xlsx::alphabetToNumber('A');
+        $this->assertEquals($result, 0);
+        $result = Xlsx::alphabetToNumber('AA');
+        $this->assertEquals($result, 26);
+        $result = Xlsx::alphabetToNumber('AM');
+        $this->assertEquals($result, 38);
+        $result = Xlsx::alphabetToNumber('AZ');
+        $this->assertEquals($result, 51);
+        $result = Xlsx::alphabetToNumber('BA');
+        $this->assertEquals($result, 52);
+        $result = Xlsx::alphabetToNumber('DL');
+        $this->assertEquals($result, 115);
+        $result = Xlsx::alphabetToNumber('ZZ');
+        $this->assertEquals($result, 701);
+        $result = Xlsx::alphabetToNumber('AAA');
+        $this->assertEquals($result, 702);
+    }
+
+    /**
+     * _setTestFile.
+     *
+     * @return
+     */
+    private function _setTestFile($fileName, $to = null)
+    {
+        if (!$fileName || !$to) {
+            return false;
+        }
+        $from = dirname(__FILE__).'/'.$fileName;
+
+        return copy($from, $to);
+    }
+}
